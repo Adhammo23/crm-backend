c0b9eb985068fc0daa624cb047575085c55a53fc
Adhammo23 <Adhammo23@users.noreply.github.com>
2026-06-24 18:06:38 +0300

    feat(user): add method to find User by email in UserRepository

--- src/main/java/com/adham/crm_backend/repository/UserRepository.java
+++ src/main/java/com/adham/crm_backend/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.adham.crm_backend.repository;
 import com.adham.crm_backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+     Optional<User> findByEmail(String email);
 }
